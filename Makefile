c1de604e3efed01cbd73341cdc108b6958d62cde
Cameron Morris <cameron.morris@uconn.edu>
2018-12-21 10:44:59 -0500

    add test make target

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 CC=g++
-CFLAGS= -Wall -g -std=c++14 -O3
+CFLAGS= -Wall -g -std=c++14
 OBJECTS := $(patsubst %.cpp,%.o,$(wildcard *.cpp))
 
 all: $(OBJECTS)
-	$(CC) $(CFLAGS) -o bgp-extrapolator $(OBJECTS) -lpqxx -lpq
+	$(CC) $(CFLAGS) -O3 -o bgp-extrapolator $(OBJECTS) -lpqxx -lpq
+
+test: $(OBJECTS)
+	$(CC) $(CFLAGS) -DRUN_TESTS -o bgp-extrapolator $(OBJECTS) -lpqxx -lpq
 
 %.o: %.cpp
 	$(CC) -c $(CFLAGS) $< -o $@
--- main.cpp
+++ main.cpp
@@ -6,32 +6,34 @@
 #include "Tests.h"
 
 int main(int argc, char *argv[]) {
-    // test code
     using namespace std;
        
-    //Start calling Test functions here 
-//    as_relationship_test();
-//    as_receive_test();
-//    as_process_test();
-//    tarjan_accuracy_test();
-//    tarjan_size_test();
-//    send_all_test();
-//    tarjan_on_real_data();
-//    combine_components_test();
-//    decide_ranks_test(); 
-//    fully_create_graph_test();
-//    give_ann_to_as_path_test();
-//    propagate_up_test();
-//    propagate_down_test();
-//    test_db_connection();
-//    select_all_test();
-//    test_db_connection();
-//    full_propagation_test_a();
-//    announcement_comparison_test();
+    #ifdef RUN_TESTS
+    //as_relationship_test();
+    //as_receive_test();
+    //as_process_test();
+    //tarjan_accuracy_test();
+    //tarjan_size_test();
+    //send_all_test();
+    //tarjan_on_real_data();
+    //combine_components_test();
+    //decide_ranks_test(); 
+    //fully_create_graph_test();
+    //give_ann_to_as_path_test();
+    //propagate_up_test();
+    //propagate_down_test();
+    //test_db_connection();
+    //select_all_test();
+    //test_db_connection();
+    //full_propagation_test_a();
+    //announcement_comparison_test();
     full_propagation_test_b();
-//    query_array_test();
-//    distinct_prefixes_test();
-//    map_test();
+    //query_array_test();
+    //distinct_prefixes_test();
+    //map_test();
     cout << "All tests run successfully." << endl;
+    #endif
+
+    // put actual main code here
     return 0;
 }
