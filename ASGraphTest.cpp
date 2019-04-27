ff0b1a32ba86d4de14d2da3d8a2696614a25d91d
James Breslin <james.breslin@uconn.edu>
2019-04-27 17:03:05 -0400

    More framework for unit tests

--- ASGraphTest.cpp
+++ ASGraphTest.cpp
@@ -1,12 +1,42 @@
 /** Unit tests for ASGraph.h and ASGraph.cpp
  */
 
-bool test_add_relationship(){}
-bool test_create_graph_from_db(){}
-bool test_remove_stubs(){}
-bool test_tarjan(){} // includes tarjan_helper()
-bool test_combine_components(){}
-bool test_save_stubs_to_db(){}
-bool test_save_non_stubs_to_db(){}
-bool test_save_supernodes_to_db(){}
+bool test_add_relationship(){
+    return true;
+}
+
+
+bool test_create_graph_from_db(){
+    return true;
+}
+
+
+bool test_remove_stubs(){
+    return true;
+}
+
+
+bool test_tarjan(){ // includes tarjan_helper()
+    return true;
+}
+
+
+bool test_combine_components(){
+    return true;
+}
+
+
+bool test_save_stubs_to_db(){
+    return true;
+}
+
+
+bool test_save_non_stubs_to_db(){
+    return true;
+}
+
+
+bool test_save_supernodes_to_db(){
+    return true;
+}
 
--- ASTest.cpp
+++ ASTest.cpp
@@ -1,28 +1,33 @@
 /** Unit tests for AS.h and AS.cpp
  */
 
-// AS.h
-
-
-
-
 // AS.cpp
 
-
+/** Test adding neighbor AS to the appropriate set based on the relationship.
+ *
+ * @return True if successful, otherwise false
+ */
 bool test_add_neighbor(){
     return true;
 }
 
 
+/** Update rank of this AS only if newrank is greater than the current rank.
+ *
+ * @return true if successful.
+ */
 bool test_update_rank(){
     return true;
 }
 
 
+/** Push the received announcements to the incoming_announcements vector. 
+ *
+ * @return true if successful.
+ */
 bool test_receive_announcement(){
     return true;
 }
-
 
 bool test_already_recieved(){
     return true;
