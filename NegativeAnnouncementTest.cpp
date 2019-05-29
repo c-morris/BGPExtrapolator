76cf0dde4bcd694055d00106bd4ef155097ac14c
Cameron Morris <cameron.morris@uconn.edu>
2019-05-29 12:27:05 -0400

    remove debug print statement

--- NegativeAnnouncementTest.cpp
+++ NegativeAnnouncementTest.cpp
@@ -7,6 +7,6 @@ bool test_nannouncement_constructor() {
     Prefix<> s("137.99.0.0", "255.255.255.0");
     NegativeAnnouncement n = NegativeAnnouncement(13796, p.addr, p.netmask, 22742, std::set<Prefix<>>());
     n.null_route_subprefix(s);
-    std::cerr << n;
+    //std::cerr << n;
     return true;
 }
