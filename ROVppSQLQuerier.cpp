24e24df43889d28addd2efbe69040695b384c4ba
Cameron Morris <cameron.morris@uconn.edu>
2019-12-02 19:55:07 -0500

    remove ROVAS class, add notes on sql querier

---
+++ ROVppSQLQuerier.cpp
@@ -0,0 +1,4 @@
+// need function to read in sql table(s) of attacker-victim pairs
+// should get justin's input on how to do the tables
+
+// also need to read in rov validity (or a list/set of attackers)
