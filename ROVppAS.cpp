4f12b1b0dfd1f4ef359b14559c9d564fa63115ec
Cameron Morris <cameron.morris@uconn.edu>
2019-12-03 20:49:51 -0500

    ROVppASGraph is now made of ROVppASes

---
+++ ROVppAS.cpp
@@ -0,0 +1,33 @@
+/*************************************************************************
+ * This file is part of the BGP Extrapolator.
+ *
+ * Developed for the SIDR ROV Forecast.
+ * This package includes software developed by the SIDR Project
+ * (https://sidr.engr.uconn.edu/).
+ * See the COPYRIGHT file at the top-level directory of this distribution
+ * for details of code ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ ************************************************************************/
+
+#include "ROVppAS.h"
+
+ROVppAS::ROVppAS(uint32_t myasn, 
+       std::map<std::pair<Prefix<>, uint32_t>,std::set<uint32_t>*> *inv, 
+       std::set<uint32_t> *prov,
+       std::set<uint32_t> *peer,
+       std::set<uint32_t> *cust) 
+    : AS(myasn, inv, prov, peer, cust)  { }
+
+ROVppAS::~ROVppAS() { }
--- Tests.h
+++ Tests.h
@@ -68,7 +68,19 @@ bool test_rovpp_propagate_up();
 bool test_rovpp_propagate_down();
 bool test_rovpp_give_ann_to_as_path();
 bool test_rovpp_send_all_announcements();
-
+bool test_rovpp_add_relationship();
+bool test_rovpp_translate_asn();
+bool test_rovpp_decide_ranks();
+bool test_rovpp_remove_stubs();
+bool test_rovpp_combine_components();
+bool test_rovpp_get_random();
+bool test_rovpp_add_neighbor();
+bool test_rovpp_remove_neighbor();
+bool test_rovpp_receive_announcements();
+bool test_rovpp_process_announcement();
+bool test_rovpp_process_announcements();
+bool test_rovpp_already_received();
+bool test_rovpp_clear_announcements();
 
 // Prototypes for SQLQuerierTest.cpp
 
